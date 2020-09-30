c8292b764775ca4aef268a614e52ebd4db01caf6
Sergey D <dshl.srp@gmail.com>
2020-09-30 10:38:33 +0300

    clashes with version option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
-import { version, description } from '../../package.json';
+// import { version, description } from '../../package.json';
 
 commander.option('-h, --help', 'output usage information');
 commander.option('-V, --version', 'output the version number');
-commander.description(description);
+commander.description("Compares two configuration files and shows a difference.");
+commander.version("1.0.0");
 
 
